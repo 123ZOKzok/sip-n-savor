02fe4b9a2b91c7c4204a96746db9983af0917b8e
user name <user email>
2022-09-09 21:49:48 +0300

    add eventlistener

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const cocktails = document.querySelector("div#cocktails")
 const searchBar = document.querySelector(".searchBar")
+
+document.addEventListener("DOMContentLoaded", () => {
+  
+    function getCocktail(name) {
+      fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
+        .then((response) => response.json())
+        .then((result) => {
+          const data = result.drinks;
+          for (let i = 0; i < 15; i++) {
+            loadDrinks(data[i]);
+            console.log(data[i]);
+          }
+        });
+    }
